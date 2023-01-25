15ba12d18c8784e2335be2f4eec4c0ff40186536
Sam Gelman <samgelman1@gmail.com>
2023-01-25 17:43:04 -0600

    Fix imports

--- metl/relative_attention.py
+++ metl/relative_attention.py
@@ -16,10 +16,10 @@ import torch.nn.functional as F
 from torch import Tensor
 from torch.nn import Linear, Dropout, LayerNorm
 import time
-
-import structure
 import networkx as nx
-import models
+
+import metl.structure as structure
+import metl.models as models
 
 
 class RelativePosition3D(nn.Module):
