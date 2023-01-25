8059ac6ff177d78a45507daea115c1e0ad8b671a
Sam Gelman <samgelman1@gmail.com>
2023-01-24 18:04:33 -0600

    Requirements and setup updates

--- setup.py
+++ setup.py
@@ -1,10 +1,4 @@
 from setuptools import setup
-setup(name='metl-pretrained',
-      version='0.1',
-      description='Mutational effect transfer learning',
-      url='https://github.com/samgelman/metl-pretrained',
-      author='Sam Gelman',
-      author_email='sgelman2@wisc.edu',
-      license='MIT',
-      packages=['metl'],
-      install_requires=['torch>=1.11.0', 'numpy>=1.23.2', 'scipy>=1.9.1', 'biopandas>=0.2.7', 'networkx>=2.6.3'])
+
+if __name__ == '__main__':
+    setup()
