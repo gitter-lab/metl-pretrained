531d195cbb5304bde208547ad633cc22327c3a38
Sam Gelman <samgelman1@gmail.com>
2023-10-09 12:47:13 -0500

    Additional examples

--- metl/test4.py
+++ metl/test4.py
@@ -8,7 +8,7 @@ def main():
     # the GB1 WT sequence
     wt = "MQYKLILNGKTLKGETTTEAVDAATAEKVFKQYANDNGVDGEWTYDDATKTFTVTE"
 
-    # some example GB1 variants to compute the Rosetta scores for
+    # some example GB1 variants for which to compute the METL-Local Rosetta scores estimates
     variants = ["T17P,T54F",
                 "V28L,F51A",
                 "T17P,V28L,F51A,T54F"]
@@ -20,7 +20,27 @@ def main():
     # no need to compute gradients for inference
     with torch.no_grad():
         predictions = model(torch.tensor(encoded_variants), pdb_fn="../pdbs/2qmt_p.pdb")
+    print(predictions)
 
+    # can also input full sequences
+    sequences = ["MPYKLILNGKTLKGETTTEAVDAATAEKVFKQYANDNGVDGEWTYDDATKTFTVTE",
+                 "MPAKLILNGKTLKGETTTEAVDAATAEKVFKQYANDNGVDGEWTYDDATKTFTVTE",
+                 "MGEKLILNGKTLKGETTTEAVDAATAEKVFKQYANDNGVDGEWTYDDATKTFTVTE"]
+    encoded_sequences = data_encoder.encode_sequences(sequences)
+    model.eval()
+    with torch.no_grad():
+        predictions = model(torch.tensor(encoded_sequences), pdb_fn="../pdbs/2qmt_p.pdb")
+    print(predictions)
+
+    # can also use the 1D model which doesn't require a PDB file
+    model, data_encoder = metl.get_from_ident("METL-L-2M-1D-GB1")
+    variants = ["T17P,T54F",
+                "V28L,F51A",
+                "T17P,V28L,F51A,T54F"]
+    encoded_variants = data_encoder.encode_variants(wt, variants)
+    model.eval()
+    with torch.no_grad():
+        predictions = model(torch.tensor(encoded_variants))
     print(predictions)
 
 
